0ad2e45ca4049872e005674f0639ea9924f19b66
Raniro Coelho <ranirocoelho@gmail.com>
2025-08-08 11:40:10 -0300

    feat: add react-query and auth store

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,23 @@
 import { Slot } from 'expo-router';
 import React from 'react';
 import { ImageBackground, View } from 'react-native';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/lib/queryClient';
 import { AuthProvider } from '@features/auth/provider';
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <ImageBackground
-        source={require('@assets/images/bg01.png')}
-        className='flex-1'
-        resizeMode='cover'>
-        <View className='flex-1 bg-white/50'>
-          <Slot />
-        </View>
-      </ImageBackground>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ImageBackground
+          source={require('@assets/images/bg01.png')}
+          className='flex-1'
+          resizeMode='cover'>
+          <View className='flex-1 bg-white/50'>
+            <Slot />
+          </View>
+        </ImageBackground>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
