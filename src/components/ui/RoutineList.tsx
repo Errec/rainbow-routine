dd2fcfc238f6d87210c84a32d8f6dab1a3cd69a5
Raniro Coelho <ranirocoelho@gmail.com>
2025-08-08 10:39:36 -0300

    refactor: add Routine interface and update props

--- src/components/ui/RoutineList.tsx
+++ src/components/ui/RoutineList.tsx
@@ -3,28 +3,35 @@ import { FlatList, View } from 'react-native';
 import { z } from 'zod';
 import RoutineListItem from './RoutineListItem';
 
+interface Routine {
+  id: number;
+  name: string;
+  type?: string;
+}
+
 const RoutineList: React.FC = () => {
-  const data = [
-    { id: 1, name: 'Routine 1' },
-    { id: 2, name: 'Routine 2' },
-    { id: 3, name: 'Routine 3' },
+  const data: Routine[] = [
+    { id: 1, name: 'Routine 1', type: 'Workout' },
+    { id: 2, name: 'Routine 2', type: 'Meditation' },
+    { id: 3, name: 'Routine 3', type: 'Nutrition' },
     // Add more routine objects as needed
   ];
 
   const routineSchema = z.object({
     id: z.number(),
     name: z.string(),
+    type: z.string().optional(),
   });
 
-  const validatedData = routineSchema.array().parse(data);
+  const validatedData: Routine[] = routineSchema.array().parse(data);
 
-  const renderItem = ({ item }: { item: { id: number; name: string } }) => (
-    <RoutineListItem id={item.id} name={item.name} type={''} />
+  const renderItem = ({ item }: { item: Routine }) => (
+    <RoutineListItem id={item.id} name={item.name} type={item.type} />
   );
 
   return (
     <View>
-      <FlatList
+      <FlatList<Routine>
         data={validatedData}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
