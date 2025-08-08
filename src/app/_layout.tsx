fd77daaa2fcdd5d57cdc80991b535ccb2c3c6777
Raniro Coelho <ranirocoelho@gmail.com>
2025-08-08 13:26:12 -0300

    feat: integrate Sentry

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,14 @@
 import { Slot } from 'expo-router';
+import * as Sentry from 'sentry-expo';
 import React from 'react';
 import { ImageBackground, View } from 'react-native';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/lib/queryClient';
 import { AuthProvider } from '@features/auth/provider';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+});
 
 export default function RootLayout() {
   return (
